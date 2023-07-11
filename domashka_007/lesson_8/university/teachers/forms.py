d390047f568dbd7450e284d95fe454069634b727
Vodymyr <129887447+Vodymyr@users.noreply.github.com>
2023-07-11 18:54:23 +0200

    Delete domashka_007/lesson_8/university directory

--- domashka_007/lesson_8/university/teachers/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from teachers.models import Teacher
-
-
-class TeacherForm(ModelForm):
-    class Meta:
-        model = Teacher
-        fields = ["first_name", "last_name", "subject"]
